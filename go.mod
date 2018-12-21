cde0e8e6c938087922aabca2a98ce0f26a1a34cc
Jason McClellan <jason@jasonmcclellan.io>
2018-12-20 23:38:17 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/jmcclell/random
+
+require (
+	github.com/go-redis/redis v6.14.2+incompatible // indirect
+	github.com/gorilla/schema v1.0.2
+	github.com/heptiolabs/healthcheck v0.0.0-20180807145615-6ff867650f40
+	github.com/kelseyhightower/envconfig v1.3.0
+	github.com/prometheus/client_golang v0.9.2
+)
---
+++ main_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRandomHandlerWithDefaults(t *testing.T) {
+	req, err := http.NewRequest("GET", "/random", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler := http.HandlerFunc(randomNumberHandler)
+
+	rand.Seed(1) // Gives series: 81, 87, 47
+	vals := [3]int{81, 87, 47}
+	for _, val := range vals {
+		rr := httptest.NewRecorder()
+		handler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusOK)
+		}
+
+		expected := fmt.Sprintf(`{"value":%d}`, val)
+		if rr.Body.String() != expected {
+			t.Errorf("handler returned unexpected body: got %v want %v",
+				rr.Body.String(), expected)
+		}
+	}
+}
+
+func TestRandomHandlerWithMinGreaterThanMax(t *testing.T) {
+	req, err := http.NewRequest("GET", "/random?min=100&max=50", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler := http.HandlerFunc(randomNumberHandler)
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusBadRequest)
+	}
+}
