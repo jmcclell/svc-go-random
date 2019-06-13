5d919f862b06facb7441f9930ef64916efb5e55e
Jason McClellan <jason@jasonmcclellan.io>
2019-06-13 08:36:13 -0400

    cleaning up old build tooling

--- build.sh
+++
@@ -1,1 +0,0 @@
-docker build -t 920858517028.dkr.ecr.us-east-1.amazonaws.com/random-docker:1.0 -f docker/Dockerfile .
--- dev.sh
+++
@@ -1,3 +0,0 @@
-docker build -t random-dev -f docker/Dockerfile-dev
-ocker run --rm -it -p 8000:3000 -v $(PWD):/go/src/app --network dev random-dev
-
